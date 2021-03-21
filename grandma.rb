bf09f58d85b1b8c13058a11380cb28856d4db933
Katherine Walendzik <katherinewalendzik@gmail.com>
2021-03-21 17:59:00 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@ def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
   #elsif phrase 
-end
+  end
 end
 
 # Whatever you say to grandma, she should respond with
