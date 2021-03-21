49cef39eddb3c4f2bb774fbb78eadd94308e6860
Katherine Walendzik <katherinewalendzik@gmail.com>
2021-03-21 17:56:05 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 def speak_to_grandma(phrase)
   if phrase == !phrase.upcase
     puts "HUH?! SPEAK UP, SONNY!"
-  elsif phrase == phrase.upcase
+  elsif phrase == phrase.upcase $$ phrase !== ""
     puts "NO, NOT SINCE 1938!"
   else
     puts "I LOVE YOU TOO PUMPKIN!"
